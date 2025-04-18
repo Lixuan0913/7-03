60fd5ed15cd2ef89292c811a7c7b2a1f21a80618
Amira Sofia <amirasazizul@yahoo.com>
2025-04-19 02:27:26 +0800

    something

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Something!'
+@app.route("/")
+def home():
+    return "Main page <h1>HOME<h1>"
 
-if __name__ == '__main__':
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+if __name__ == "__main__":
     app.run(debug=True)
