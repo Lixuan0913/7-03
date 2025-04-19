cc5beda40fde50d265dd6599f68ed50ceb07fb96
Amira Sofia <amirasazizul@yahoo.com>
2025-04-20 03:46:10 +0800

    web test

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Main page <h1>HOME<h1>"
-
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name="Admin!"))
+    return render_template("index.html", content="Testing")
 
 if __name__ == "__main__":
     app.run(debug=True)
